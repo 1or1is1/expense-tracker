a001c57e47f95074f1224895cacc2996d457ee9c
Ankit <kankit476@gmail.com>
2022-05-01 12:32:30 +0530

    Added List of Expenses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const expenses = [
+    { date: new Date(2022, 1, 23), title: 'Laptop', amount: 1234.89 },
+    { date: new Date(2022, 2, 11), title: 'Grocery', amount: 234.9 },
+    { date: new Date(2022, 2, 21), title: 'Investment', amount: 10000 },
+    { date: new Date(2022, 3, 16), title: 'Partying', amount: 123.99 },
+  ]
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
