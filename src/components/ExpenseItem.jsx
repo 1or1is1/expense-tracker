5591f5c8778e309328ce7dea18f0618d2320cd0d
Ankit <kankit476@gmail.com>
2022-05-01 12:45:47 +0530

    Added a wrapper Card Component so that we can use card component with any of the component where it is needed.

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,6 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 const ExpenseItem = (prop) => {
     const expenseDate = prop.date;
@@ -7,13 +8,13 @@ const ExpenseItem = (prop) => {
     const expenseAmount = prop.amount;
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={expenseDate}/>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
