053ce5d77031c403e1a69e6f44ebf1f272e0d8f7
Ankit <kankit476@gmail.com>
2022-05-07 20:00:27 +0530

    Added logic to Add New Expense Form

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -8,13 +8,15 @@ const ExpenseItem = (prop) => {
     const expenseAmount = prop.amount;
 
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={expenseDate}/>
-            <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={expenseDate} />
+                <div className='expense-item__description'>
+                    <h2>{expenseTitle}</h2>
+                    <div className='expense-item__price'>${expenseAmount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,10 +1,17 @@
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+    let [showExpenseForm, setShowExpenseForm] = useState(false);
+
+    const addNewExpenseHandler = () => {
+        setShowExpenseForm(prevVal => showExpenseForm = !prevVal);
+    }
+
     const addExpenseHandler = (data) => {
         const payload = {
-            id: Math.random().toFixed(2)*100,
+            id: Math.random().toFixed(2) * 100,
             ...data,
         }
         props.onAddNewExpense(payload);
@@ -12,7 +19,9 @@ const NewExpense = (props) => {
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onAddExpense={addExpenseHandler} />
+            {showExpenseForm 
+            ? <ExpenseForm onAddExpense={addExpenseHandler} onCancelButton={addNewExpenseHandler} /> 
+            : <button type='button' onClick={addNewExpenseHandler}>Add New Expense</button>}
         </div>
     );
 }
